52c3c3b2ab6458b9e1060c86599cbb32ec7f7410
Gabriel <g_szybiak@wp.pl>
2023-09-12 15:05:38 +0200

    AW3 WebSite Mercedes

---
+++ src/test/java/firstHtmlWebsite/AW3/Aw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package firstHtmlWebsite.AW3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aw4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
